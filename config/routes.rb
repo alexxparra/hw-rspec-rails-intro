389aac490f5cf83089c2a26bfd8a863099a17a91
Alexander Parra <alexparra@berkeley.edu>
2021-11-10 04:49:55 +0000

    i hate this project

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rottenpotatoes::Application.routes.draw do
     # map '/' to be a redirect to '/movies'
     root :to => redirect('/movies')
     get '/search_tmdb', to: 'movies#search_tmdb'
-    post '/add_movie', to: 'movies#add_movie'
+#     post '/add_movie', to: 'movies#add_movie'
+    get '/add_movie', to: 'movies#add_movie'
   end
   
